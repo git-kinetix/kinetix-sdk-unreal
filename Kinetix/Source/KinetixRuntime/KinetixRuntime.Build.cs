f470c8ade6663025c93e8a4c38fbdb77de03c0ad
Alexandre MARIE <alexandre_marie@outlook.com>
2023-09-14 18:19:05 +0200

    Update KinetixRuntime.Build.cs

--- Kinetix/Source/KinetixRuntime/KinetixRuntime.Build.cs
+++ Kinetix/Source/KinetixRuntime/KinetixRuntime.Build.cs
@@ -25,7 +25,7 @@ public class KinetixRuntime : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core"
+				"Core",
 				"glTFRuntime",
 				"PoseSearch"
 				// ... add other public dependencies that you statically link with here ...
